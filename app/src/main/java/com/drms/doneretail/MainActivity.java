c0fe623e7071b005ca39f706aacddc73baacc945
Thapelo <27338053@student.g.nwu.ac.za>
2019-09-24 19:14:30 +0200

    adapter

--- app/src/main/java/com/drms/doneretail/MainActivity.java
+++ app/src/main/java/com/drms/doneretail/MainActivity.java
@@ -1,6 +1,7 @@
 package com.drms.doneretail;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -9,10 +10,23 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<modelData> dashModelArrayList;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.rvl);
+
+        dashModelArrayList = new ArrayList<>();
+        String heads[] = {"Warehouse Management", "Store Room Management", "Shelf Management", "Point of Sale", "Settings"};
+        int Images[] = {R.drawable.warehousing, R.drawable.stock, R.drawable.shelf, R.drawable.pos, R.drawable.logo};
+
+        for(int i = 0; i < heads.length; i++)
+        {
+            modelData dataModel = new modelData();
+            dataModel.setHead(heads[i]);
+            dataModel.setImage(Images[i]);
+        }
     }
 }
--- app/src/main/java/com/drms/doneretail/dashAdapter.java
+++ app/src/main/java/com/drms/doneretail/dashAdapter.java
@@ -1,5 +1,6 @@
 package com.drms.doneretail;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,30 +9,55 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class dashAdapter extends RecyclerView.Adapter<dashAdapter.ViewHolder> {
+
+    ArrayList<modelData> dashModelArrayList;
+
+    public dashAdapter(ArrayList<modelData> dashModelArrayList) {
+        this.dashModelArrayList = dashModelArrayList;
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent,false);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        String ret_head = dashModelArrayList.get(position).getHead();
+        holder.setHeader(ret_head);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return dashModelArrayList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         TextView head;
         ImageView images;
+        View myView;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            myView = itemView;
+        }
+
+        public void setHeader(String h)
+        {
+            head = myView.findViewById(R.id.header);
+            head.setText(h);
+        }
+
+        public void setImage(int i)
+        {
+            images = myView.findViewById(R.id.image_id);
+            images.setImageResource(i);
         }
     }
 }
