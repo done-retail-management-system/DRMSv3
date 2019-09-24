9b742407e0ea8bf8031e92d487a9b995a561da57
Thapelo <27338053@student.g.nwu.ac.za>
2019-09-24 10:55:21 +0200

    connecting to cognito

--- app/src/main/java/com/drms/doneretail/AWSCognito.java
+++ app/src/main/java/com/drms/doneretail/AWSCognito.java
@@ -1,4 +1,69 @@
 package com.drms.doneretail;
 
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoDevice;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserAttributes;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserDetails;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserPool;
+import com.amazonaws.mobileconnectors.cognitoidentityprovider.CognitoUserSession;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.cognitoidentityprovider.model.AttributeType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 public class AWSCognito {
+
+    private static AWSCognito awsCognito;
+    private static CognitoUserPool userPool;
+    private static String user;
+    private static CognitoDevice newDevice;
+
+    private static CognitoUserAttributes attributesChanged;
+    private static List<AttributeType> attributesToDelete;
+
+    private static List<ItemToDisplay> currDisplayedItems;
+    private static  int itemCount;
+
+    private static List<ItemToDisplay> trustedDevices;
+    private static int trustedDevicesCount;
+    private static List<CognitoDevice> deviceDetails;
+    private static CognitoDevice thisDevice;
+    private static boolean thisDeviceTrustState;
+
+    private static List<ItemToDisplay> firstTimeLogInDetails;
+    private static Map<String, String> firstTimeLogInUserAttributes;
+    private static List<String> firstTimeLogInRequiredAttributes;
+    private static int firstTimeLogInItemsCount;
+    private static Map<String, String> firstTimeLogInUpDatedAttributes;
+    private static String firstTimeLoginNewPassword;
+
+    private static List<ItemToDisplay> mfaOptions;
+    private static List<String> mfaAllOptionsCode;
+
+    private static final String userPoolId = "us-east-2_wqOUvI6lL";
+
+    private static final String clientId = "70c3bvo3mqpuveu0vi7vegjl68";
+
+    private static final String clientSecret = "1bcp6ok3tsnrsga3ekc0u8gh4dtl1pil6lj0m6mgj3r6429oehi5";
+
+    private static final Regions cognitoRegion = Regions.US_EAST_2;
+
+    // User details from the service
+    private static CognitoUserSession currSession;
+    private static CognitoUserDetails userDetails;
+
+    // User details to display - they are the current values, including any local modification
+    private static boolean phoneVerified;
+    private static boolean emailVerified;
+
+    private static boolean phoneAvailable;
+    private static boolean emailAvailable;
+
+    private static Set<String> currUserAttributes;
+
+
 }
