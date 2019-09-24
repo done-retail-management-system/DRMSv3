30fa5edc24b35cd3384b3a531289b61cc95f1d4f
Thapelo <27338053@student.g.nwu.ac.za>
2019-09-24 11:15:27 +0200

    Methods for SMS protocol

--- app/src/main/java/com/drms/doneretail/LoginActivity.java
+++ app/src/main/java/com/drms/doneretail/LoginActivity.java
@@ -2,9 +2,11 @@ package com.drms.doneretail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -40,6 +42,7 @@ public class LoginActivity extends AppCompatActivity {
     private ForgotPasswordContinuation forgotPasswordContinuation;
     private NewPasswordContinuation newPasswordContinuation;
     private ChooseMfaContinuation mfaOptionsContinuation;
+    private final String TAG="LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,9 +50,126 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
+        AWSCognito.init(getApplicationContext());
+        initApp();
+        findCurrent();
+    }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        switch (requestCode) {
+            case 1:
+                // Register user
+                if(resultCode == RESULT_OK) {
+                    String name = data.getStringExtra("name");
+                    if (!name.isEmpty()) {
+                        userText.setText(name);
+                        passText.setText("");
+                        passText.requestFocus();
+                    }
+                    String userPasswd = data.getStringExtra("password");
+                    if (!userPasswd.isEmpty()) {
+                        passText.setText(userPasswd);
+                    }
+                    if (!name.isEmpty() && !userPasswd.isEmpty()) {
+                        // We have the user details, so sign in!
+                        username = name;
+                        password = userPasswd;
+                        AWSCognito.getPool().getUser(username).getSessionInBackground(authenticationHandler);
+                    }
+                }
+                break;
+            case 2:
+                // Confirm register user
+                if(resultCode == RESULT_OK) {
+                    String name = data.getStringExtra("name");
+                    if (!name.isEmpty()) {
+                        userText.setText(name);
+                        passText.setText("");
+                        passText.requestFocus();
+                    }
+                }
+                break;
+            case 3:
+                // Forgot password
+                if(resultCode == RESULT_OK) {
+                    String newPass = data.getStringExtra("newPass");
+                    String code = data.getStringExtra("code");
+                    if (newPass != null && code != null) {
+                        if (!newPass.isEmpty() && !code.isEmpty()) {
+                            showWaitDialog("Setting new password...");
+                            forgotPasswordContinuation.setPassword(newPass);
+                            forgotPasswordContinuation.setVerificationCode(code);
+                            forgotPasswordContinuation.continueTask();
+                        }
+                    }
+                }
+                break;
+            case 4:
+                // User
+                if(resultCode == RESULT_OK) {
+                    clearInput();
+                    String name = data.getStringExtra("TODO");
+                    if(name != null) {
+                        if (!name.isEmpty()) {
+                            name.equals("exit");
+                            onBackPressed();
+                        }
+                    }
+                }
+                break;
+            case 5:
+                //MFA
+                closeWaitDialog();
+                if(resultCode == RESULT_OK) {
+                    String code = data.getStringExtra("mfacode");
+                    if(code != null) {
+                        if (code.length() > 0) {
+                            showWaitDialog("Signing in...");
+                            multiFactorAuthenticationContinuation.setMfaCode(code);
+                            multiFactorAuthenticationContinuation.continueTask();
+                        } else {
+                            passText.setText("");
+                            passText.requestFocus();
+                        }
+                    }
+                }
+                break;
+            case 6:
+                //New password
+                closeWaitDialog();
+                Boolean continueSignIn = false;
+                if (resultCode == RESULT_OK) {
+                    continueSignIn = data.getBooleanExtra("continueSignIn", false);
+                }
+                if (continueSignIn) {
+                    continueWithFirstTimeSignIn();
+                }
+                break;
+            case 7:
+                // Choose MFA
+                closeWaitDialog();
+                if(resultCode == RESULT_OK) {
+                    String option = data.getStringExtra("mfaOption");
+                    if (option != null) {
+                        if (option.length() > 0) {
+                            Log.d(TAG, " -- Selected Option: " + option);
+                            conitnueWithSelectedMfa(option);
+                        } else {
+                            passText.setText("");
+                            passText.requestFocus();
+                            TextView label = (TextView) findViewById(R.id.textViewUserPasswordMessage);
+                            label.setText("Login cancelled");
+                        }
+                    }
+                }
+        }
     }
 
 
