69e0b9945e7bf163fc9015288b82813e3ff89f1a
Thapelo <27338053@student.g.nwu.ac.za>
2019-09-24 18:11:54 +0200

    dashmodelarraylist

---
+++ app/src/main/java/com/drms/doneretail/dashAdapter.java
@@ -0,0 +1,37 @@
+package com.drms.doneretail;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class dashAdapter extends RecyclerView.Adapter<dashAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView head;
+        ImageView images;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
