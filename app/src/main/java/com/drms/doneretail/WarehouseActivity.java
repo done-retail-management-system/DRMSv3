3193db30ca00a9941a7ea64dffea8b13a023d6f2
Boits <26496666@student.g.nwu.ac.za>
2019-09-27 15:52:22 +0200

    Navigation

--- app/src/main/java/com/drms/doneretail/WarehouseActivity.java
+++ app/src/main/java/com/drms/doneretail/WarehouseActivity.java
@@ -26,10 +26,67 @@ public class WarehouseActivity extends AppCompatActivity {
                 openStockActivity();
             }
         });
+
+        button = (Button) findViewById(R.id.btn_order);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openOrdersActivity();
+            }
+        });
+
+        button = (Button) findViewById(R.id.btn_supplier);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSupplierActivity();
+            }
+        });
+
+        button = (Button) findViewById(R.id.btn_shops);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openShopActivity();
+            }
+        });
+
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStockActivity();
+                openOrdersActivity();
+                openSupplierActivity();
+                openShopActivity();
+            }
+        });
+
     }
 
+
+    //Methods to open  each activity
     public void openStockActivity(){
         Intent intent = new Intent(this, StockActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void openOrdersActivity(){
+        Intent intent = new Intent(this, OrdersActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void openSupplierActivity(){
+        Intent intent = new Intent(this, SuppliersActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void openShopActivity(){
+        Intent intent = new Intent(this, ShopsActivity.class);
         startActivity(intent);
 
     }
