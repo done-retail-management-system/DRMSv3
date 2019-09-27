cca7dd7d8c8f83ebad59f0a67f9704653106ab1d
Boits <26496666@student.g.nwu.ac.za>
2019-09-27 20:51:56 +0200

    Previous commit re-evaluated lmao!

--- app/src/main/java/com/drms/doneretail/WarehouseActivity.java
+++ app/src/main/java/com/drms/doneretail/WarehouseActivity.java
@@ -23,11 +23,11 @@ public class WarehouseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getSupportActionBar().hide();
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //getSupportActionBar().hide();
 
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                //WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_warehouse);
         Calendar calendar =  Calendar.getInstance();
