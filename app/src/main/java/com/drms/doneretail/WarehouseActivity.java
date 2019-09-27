b024d080bfa8445167a2355eca46383f8792348e
Boits <26496666@student.g.nwu.ac.za>
2019-09-27 20:43:52 +0200

    Removed default toolbar from Warehouse division

--- app/src/main/java/com/drms/doneretail/WarehouseActivity.java
+++ app/src/main/java/com/drms/doneretail/WarehouseActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.text.DateFormat;
@@ -19,13 +21,19 @@ public class WarehouseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_warehouse);
-
-
         Calendar calendar =  Calendar.getInstance();
         String currentDate = DateFormat.getDateInstance().format(calendar.getTime());
-        TextView textViewDate = findViewById(R.id.date);
-        textViewDate.setText(currentDate);
+        //TextView textViewDate = findViewById(R.id.date);
+        //textViewDate.setText(currentDate);
         //button = button.findViewById(R.id.btn_stock);
 
         button = (Button) findViewById(R.id.btn_stock);
