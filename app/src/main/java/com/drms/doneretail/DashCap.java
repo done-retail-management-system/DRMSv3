0f2a489814c003e3e6c13edda84b67f66ac73fef
Nomonde Makgare <27378993@student.g.nwu.ac.za>
2019-09-28 09:47:45 +0200

    GUI continuation

--- app/src/main/java/com/drms/doneretail/DashCap.java
+++ app/src/main/java/com/drms/doneretail/DashCap.java
@@ -7,21 +7,61 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
-import androidx.appcompat.app.AlertController;
+//import androidx.appcompat.app.AlertController;
 import androidx.recyclerview.widget.RecyclerView;
+
 
 import java.util.ArrayList;
 
 public class DashCap extends RecyclerView.Adapter<DashCap.ViewHolder> {
 
-    ArrayList<DataS> dashList;
+    ArrayList<DataS> dashlist;
 
     public DashCap(ArrayList<DataS> dashList) {
-        this.dashList = dashList;
+        this.dashlist = dashlist;
     }
-
-
     @NonNull
     @Override
-    pub
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_shelf__dash, parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String ret_head = dashlist.get(position).getTop();
+        holder.setHeader(ret_head);
+
+        int ret_img = dashlist.get(position).getPic();
+        holder.setImage(ret_img);
+    }
+
+    @Override
+    public int getItemCount() {
+        return dashlist.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView top;
+        ImageView pic;
+        View myView;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            myView = itemView;
+        }
+
+        public void setHeader(String h)
+        {
+            top = myView.findViewById(R.id.header);
+            top.setText(h);
+        }
+
+        public void setImage(int i)
+        {
+            pic = myView.findViewById(R.id.image_id);
+            pic.setImageResource(i);
+        }
+    }
 }
