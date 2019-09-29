bca7a47913d92f5cf0f5b2873a64481cb17bba12
Nomonde Makgare <27378993@student.g.nwu.ac.za>
2019-09-29 18:20:24 +0200

    Shimmer Activity  for the Categories

---
+++ app/src/main/java/com/drms/doneretail/recycler_category_shimmer.java
@@ -0,0 +1,14 @@
+package com.drms.doneretail;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recycler_category_shimmer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler_category_shimmer);
+    }
+}
