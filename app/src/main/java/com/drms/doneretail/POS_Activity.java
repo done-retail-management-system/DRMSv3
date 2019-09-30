941edf8cf720c83e1c90e1b06bf7a0691433c040
Patrick Ali <29317223@student.g.nwu.ac.za>
2019-10-01 01:24:08 +0200

    Database Connection

--- app/src/main/java/com/drms/doneretail/POS_Activity.java
+++ app/src/main/java/com/drms/doneretail/POS_Activity.java
@@ -9,8 +9,13 @@ import android.view.WindowManager;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
+
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -18,7 +23,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 public class POS_Activity extends AppCompatActivity{
 
     public static String barcode;
+    ListView item_cart;
     FloatingActionButton scan_item;
+    SimpleAdapter adapt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +37,27 @@ public class POS_Activity extends AppCompatActivity{
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_pos_);
 
+        item_cart = findViewById(R.id.list_Cart);
         scan_item = findViewById(R.id.btnScan);
         scan_item.setOnClickListener(new View.OnClickListener(){
 
             @Override
             public void onClick(View v) {
-               startActivity(new Intent(getApplicationContext(), ScannerBarcode.class));
+               //startActivity(new Intent(getApplicationContext(), ScannerBarcode.class));
+                List<Map<String,String>> data = null;
+                CheckItem getData = new CheckItem();
+                data = getData.doInBackground();
+
+                String[] retrieve_From = {"Sales Number", "Date", "Total Price"};
+                int[] viewLocation = new int[]{R.id.txtID, R.id.txtdate, R.id.txtTotal};
+                adapt = new SimpleAdapter(POS_Activity.this,data,R.layout.listview,retrieve_From,viewLocation);
+
+                item_cart.setAdapter(adapt);
             }
         });
+
+
+
     }
 
 }
