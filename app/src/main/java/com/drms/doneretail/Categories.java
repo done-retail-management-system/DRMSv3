2dd4dceeaa487be000224df40b6b3602a07f7de4
Nomonde Makgare <27378993@student.g.nwu.ac.za>
2019-10-01 19:49:26 +0200

    Trying to fix my errors

---
+++ app/src/main/java/com/drms/doneretail/Categories.java
@@ -0,0 +1,26 @@
+package com.drms.doneretail;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Categories {
+
+    @SerializedName("categories")
+    @Expose
+    private List<Category> categories;
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        this.categories = categories;
+    }
+
+
+    
+    }
+
+}
