26187079b9ee876fc80ddbdee9a8a9bb378eb125
Thapelo <27338053@student.g.nwu.ac.za>
2019-10-02 04:37:20 +0200

    dash complete

--- app/src/main/java/com/drms/doneretail/MainActivity.java
+++ app/src/main/java/com/drms/doneretail/MainActivity.java
@@ -2,7 +2,10 @@ package com.drms.doneretail;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void warehouse(View view) {
+        Toast.makeText(getApplicationContext(), "I work from WAREHOUSE", Toast.LENGTH_LONG).show();
+    }
+
+    public void store(View view) {
+        openStoreroom();
+    }
+
+    public void shelf(View view) {
+        Toast.makeText(getApplicationContext(), "I work from SHELF", Toast.LENGTH_LONG).show();
+    }
+
+    public void pos(View view) {
+        Toast.makeText(getApplicationContext(), "I work from POS", Toast.LENGTH_LONG).show();
+    }
+
+    public void openStoreroom()
+    {
+        Intent storeIntent = new Intent(this, storeroomDash.class);
+        startActivity(storeIntent);
+        Toast.makeText(getApplicationContext(), "Welcome to Storeroom Management", Toast.LENGTH_LONG).show();
     }
 }
