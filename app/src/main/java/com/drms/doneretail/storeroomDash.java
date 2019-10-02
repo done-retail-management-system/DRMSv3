76a4faf570084faef77be093000d8bff66cb0d36
Thapelo <27338053@student.g.nwu.ac.za>
2019-10-02 03:32:17 +0200

    wiring

--- app/src/main/java/com/drms/doneretail/storeroomDash.java
+++ app/src/main/java/com/drms/doneretail/storeroomDash.java
@@ -3,6 +3,8 @@ package com.drms.doneretail;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class storeroomDash extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class storeroomDash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storeroom_dash);
+    }
+
+
+    public void checkIn(View view) {
+        Toast.makeText(getApplicationContext(),"I span",Toast.LENGTH_LONG).show();
+
     }
 }
