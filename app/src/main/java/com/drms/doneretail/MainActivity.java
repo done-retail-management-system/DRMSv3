4fb064b84b72ff1d828714b1f03c655332295222
Thapelo <27338053@student.g.nwu.ac.za>
2019-10-24 01:40:49 +0200

    issues

--- app/src/main/java/com/drms/doneretail/MainActivity.java
+++ app/src/main/java/com/drms/doneretail/MainActivity.java
@@ -26,29 +26,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView = findViewById(R.id.rcyl);
 
-        dashList = new ArrayList<>(); // set the Dashboard CardView
-        String top[] = {"Stock On Hand", "Shelf Management", "o"};
-        int pic[] = {R.drawable.instock, R.drawable.on_shelf, R.drawable.outofstock};
-
-        for(int i = 0; i < top.length; i++)
-        {
-            DataS dataModel = new DataS();
-            dataModel.setTop(top[i]);
-            dataModel.setPic(pic[i]);
-            dashList.add(dataModel);
-        }// Loop for items on the dash
-
-        recyclerView.setLayoutManager(new GridLayoutManager(getApplicationContext(),2));
-        cap = new DashCap();
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setAdapter(cap);
     }
 
 
     public void warehouse(View view) {
         Toast.makeText(getApplicationContext(), "I work from WAREHOUSE", Toast.LENGTH_LONG).show();
+        Intent wareIntent = new Intent(this, WarehouseActivity.class);
+        startActivity(wareIntent);
     }
 
     public void store(View view) {
