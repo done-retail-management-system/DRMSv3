d3e22d29f8bf8dddc5a89702700edaf385b81ff6
Patrick Molusi <29317223@student.g.nwu.ac.za>
2019-10-25 00:23:14 +0200

    Shelf

---
+++ app/src/main/java/com/drms/doneretail/shelf.java
@@ -0,0 +1,15 @@
+package com.drms.doneretail;
+
+public class shelf {
+    private int barcode_shelf;
+
+    public shelf(int barcode_shelf) {
+        this.barcode_shelf = barcode_shelf;
+    }
+    public int getBarcode_shelf() {
+        return barcode_shelf;
+    }
+    public void setBarcode_shelf(int barcode_shelf) {
+        this.barcode_shelf = barcode_shelf;
+    }
+}
